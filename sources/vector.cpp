2f428711a70d0ae1adef1d5623142d5cdf67aa10
Kutyirov <37041655+Kutyirov@users.noreply.github.com>
2018-03-28 17:56:26 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -7,18 +7,29 @@ vector_t::vector_t()
 {
 	size_ = 0;
 	capacity_ = 0;
+	elements_ = nullptr;
 }
 
 vector_t::vector_t(vector_t const & other)
 {
 	size_ = other.size_;
+	capacity_ = other.capacity_;
+	elements_ = new int[capacity_];
 	for (int i = 0; i < size_; i++)
 		elements_[i] = other.elements_[i];
-	capacity_ = other.capacity_;
 }
 
 vector_t & vector_t::operator =(vector_t const & other)
 {
+	if (elements_ != other.elements_) {
+		size_ = other.size_;
+		capacity_ = other.capacity_;
+		delete [] elements_;
+		elements_ = new int[capacity_];
+		for (int i = 0; i < size-; i++)
+			elements_[i] = other.elements_[i];
+	}
+			
 	return *this;
 }
 
@@ -61,12 +72,23 @@ void vector_t::push_back(int value)
 	size_++;
 	delete[] elements_;
 	int * elements_ = new int[capacity_];
-	elements_ = elements_2;
+	for (int i = 0; i < size_; i++)
+		elements_[i] = elements_2[i];
 	delete[] elements_2;
 }
 
 void vector_t::pop_back()
 {
+	size_ = size_ - 1;
+	if (size_ * 4 <= capacity_) capacity_ = capacity_ / 4;
+	int* elements_2 = new int[capacity_];
+	for (int i = 0; i < size_; i++)
+		elements_2[i] = elements_[i];
+	delete[] elements_;
+	int * elements_ = new int[capacity_];
+	for (int i = 0; i < size_; i++)
+		elements_[i] = elements_2[i];
+	delete[] elements_2;
 }
 
 int & vector_t::operator [](std::size_t index)
@@ -83,4 +105,3 @@ bool operator !=(vector_t const & lhs, vector_t const & rhs)
 {
 	return true;
 }
-//
