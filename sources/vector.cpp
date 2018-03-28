f967e53cb7bdd2491ef71d733f744b29f4e2ef48
Kutyirov <37041655+Kutyirov@users.noreply.github.com>
2018-03-28 17:58:11 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -26,7 +26,7 @@ vector_t & vector_t::operator =(vector_t const & other)
 		capacity_ = other.capacity_;
 		delete [] elements_;
 		elements_ = new int[capacity_];
-		for (int i = 0; i < size-; i++)
+		for (int i = 0; i < size_; i++)
 			elements_[i] = other.elements_[i];
 	}
 			
