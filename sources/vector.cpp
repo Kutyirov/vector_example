cff329c28b4c8893b99272d167b4621bc85ad41a
Kutyirov <37041655+Kutyirov@users.noreply.github.com>
2018-03-28 17:08:28 +0300

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -5,15 +5,16 @@
 
 vector_t::vector_t()
 {
-	size_=0;
-	capacity_=0;
+	size_ = 0;
+	capacity_ = 0;
 }
 
 vector_t::vector_t(vector_t const & other)
 {
-	elements_=other.elements_;
-	size_=other.size_;
-	capacity_=other.capacity_;
+	size_ = other.size_;
+	for (int i = 0; i < size_; i++)
+		elements_[i] = other.elements_[i];
+	capacity_ = other.capacity_;
 }
 
 vector_t & vector_t::operator =(vector_t const & other)
@@ -28,23 +29,27 @@ bool vector_t::operator ==(vector_t const & other) const
 
 vector_t::~vector_t()
 {
-	size_=0;
-	capacity_=0;
+	size_ = 0;
+	capacity_ = 0;
 	delete [] elements_;
 }
 
 std::size_t vector_t::size() const
 {
-    return size_;
+	return size_;
 }
 
 std::size_t vector_t::capacity() const
 {
-    return capacity_;
+	return capacity_;
 }
 
 void vector_t::push_back(int value)
 {
+	if (size_ == capacity_) capacity_ *= 2;
+	size_++;
+	delete[] elements_;
+	elements_ = new int [capacity_];
 }
 
 void vector_t::pop_back()
